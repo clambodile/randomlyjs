4591bb7ecbdcdac0916f08b2c62450e7fd1350c3
John Jacob <jacobjohnrobert@gmail.com>
2018-04-25 17:57:20 -0500

    write basic tests

---
+++ test.js
@@ -0,0 +1,26 @@
+const test = require('tape')
+const randomly = require('./randomly')
+
+test('return each alternative roughly half the time', function (t) {
+  t.plan(2)
+  let choices = [0, 0]
+  for (let i = 0; i < 100; i++) {
+    let choice = randomly(0, 1)
+    choices[choice]++
+  }
+  t.ok(choices[0] > 30, 'first alternative chosen close to half the time')
+  t.ok(choices[1] > 30, 'second alternative chosen close to half the time')
+})
+
+test('does not return any alternative overwhelmingly often', function (t) {
+  let choices = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  t.plan(11)
+
+  for (let i = 0; i < 100; i++) {
+    let choice = randomly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  }
+
+  choices.forEach(choice => {
+    t.ok(choice < 20)
+  })
+})
